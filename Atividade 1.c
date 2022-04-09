00d6f804618340d8bcce99d9b8c1092952b72608
Carlise Garbin Debona <92445126+Carlisegd@users.noreply.github.com>
2022-04-09 19:40:11 -0300

    Update Atividade 1.c

--- Atividade 1.c
+++ Atividade 1.c
@@ -2,35 +2,32 @@
 #include<locale.h>
 #define QTD_DADOS 50
 // meida de veiculos, media de cidades com menos de 2k veiculos, maior e a  menor taxa de acidentes e qual cidade
-// codigo da cidade, número de acidentes, número de veículos
-
-
+// codigo da cidade, nÃºmero de acidentes, nÃºmero de veÃ­culos
 // - ler codigo cidade, qtd de acidentes por cidade, e qtd de veiculos
-// - mostrar cidade co menor numero de acidentes e com maior, média de carros do total de todas as ciaddes, média de acidentes
+// - mostrar cidade co menor numero de acidentes e com maior, mÃ©dia de carros do total de todas as ciaddes, mÃ©dia de acidentes
 // das cidades q tem menos de 2000 veiculos.
 int main (){
 
     int cod_cidade[QTD_DADOS], num_veiculos[QTD_DADOS], num_acidentes[QTD_DADOS], menor, maior, qtd_cadastros, qtd_soma2;
     int cod_cidade_menor, cod_cidade_maior;
     int soma_veiculos, soma_veiculos2;
-    //media_veiculos2 - refer-se a media a penas das ciadades com menos de 2000 veículos
-
+    
     setlocale(LC_ALL, "Portuguese");
 
 
-    printf ("Quantas cidades você deseja cadastrar: \n");
+    printf ("Quantas cidades vocÃª deseja cadastrar: \n");
     scanf("%d", &qtd_cadastros);
     
 int i = 0;
     for (i = 0; i < qtd_cadastros; i++){
 
-        printf ("Digite o código da cidade: \n");
+        printf ("Digite o cÃ³digo da cidade: \n");
         scanf("%d", &cod_cidade[i]);
 
-        printf ("Digite o número de veículos: \n");
+        printf ("Digite o nÃºmero de veÃ­culos: \n");
         scanf("%d", &num_veiculos[i]);
 
-        printf ("Digite o número de acidentes de trânsito com vítimas: \n");
+        printf ("Digite o nÃºmero de acidentes de trÃ¢nsito com vÃ­timas: \n");
         scanf("%d", &num_acidentes[i]);
 
     }
@@ -68,17 +65,17 @@ int i = 0;
 
     printf("\n\n... RESULTADO PESQUISA ESTADO DO PARANA ... \n\n");
 
-    printf ("a soma dos números é: %d \n", soma_veiculos);
+    printf ("a soma dos nÃºmeros Ã©: %d \n", soma_veiculos);
 
-    printf ("a soma dos acidentes é: %d \n", soma_veiculos2);
+    printf ("a soma dos acidentes Ã©: %d \n", soma_veiculos2);
 
-    printf ("A média de veículos nas cidades é: %.2f \n", (float) (soma_veiculos/qtd_cadastros));
+    printf ("A mÃ©dia de veÃ­culos nas cidades Ã©: %.2f \n", (float) (soma_veiculos/qtd_cadastros));
 
-    printf ("A média do número de acidentes nas cidades com menos de 2000 veiculos é: %.2f \n", (float) (soma_veiculos2/qtd_soma2));
+    printf ("A mÃ©dia do nÃºmero de acidentes nas cidades com menos de 2000 veiculos Ã©: %.2f \n", (float) (soma_veiculos2/qtd_soma2));
 
-    printf("O menor número de acidentes foi %d na cidade %d \n", menor, cod_cidade_menor);
+    printf("O menor nÃºmero de acidentes foi %d na cidade %d \n", menor, cod_cidade_menor);
 
-    printf("O maior número de acidentes foi %d na cidade %d", maior, cod_cidade_maior);
+    printf("O maior nÃºmero de acidentes foi %d na cidade %d", maior, cod_cidade_maior);
 
 
 
